250a30dea5a15f2c1ba03a45b834541a6e41c3b3
lefuller <fullernle@gmail.com>
2022-01-07 15:40:20 -0800

    implements recursive linked list find

---
+++ 013-linked-list-find.js
@@ -0,0 +1,15 @@
+// class Node {
+//   constructor(val) {
+//     this.val = val;
+//     this.next = null;
+//   }
+// }
+
+const linkedListFind = (head, target) => {
+  if (head === null) return false;
+  if (head.val === target) return true;
+
+  return linkedListFind(head.next, target);
+};
+
+// T: O(n), S: O(n)

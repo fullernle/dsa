f966da50c6893cf535da4f56d2dd4d6432d44e06
Fuller Le <fullernle@gmail.com>
2022-01-14 16:01:06 -0800

    implements recursive zip lists

--- 016-zip-list.js
+++ 016-zip-list.js
@@ -24,3 +24,17 @@ const zipperLists = (head1, head2) => {
 
 	return head;
 }
+
+// Recursive Implementation
+const recursiveZip = (head1, head2) => {
+	if (head1 === null && head2 === null) return null; 
+	if (head1 == null) return head2; 
+	if (heade2 = null) return head1; 
+
+	const next1 = head1.next;
+	const next2 = head2.next; 
+
+	head1.next = head2; 
+	head2.next = recursiveZip(next1, next2);
+	return head1;
+}
